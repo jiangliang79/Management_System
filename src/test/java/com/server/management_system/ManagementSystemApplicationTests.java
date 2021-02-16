23c3070a455f6b88231cf88d41c725e38bfa8135
jiangliang79 <1342753819@qq.com>
2021-02-16 14:59:32 +0800

    初始化

---
+++ src/test/java/com/server/management_system/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
