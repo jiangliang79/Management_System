6434fab99684a86c0ed248239c097e8ef1d9b108
jiangliang <jiangliang@kuaishou.com>
2021-02-21 01:48:15 +0800

    登录功能开发完成

--- src/main/java/com/server/management_system/constant/ApiConst.java
+++ src/main/java/com/server/management_system/constant/ApiConst.java
@@ -5,6 +5,6 @@ package com.server.management_system.constant;
  * Created on 2021-02-13
  */
 public interface ApiConst {
-    String AUTHENTICATION_TOKEN_HEADER_KEY = "Authentication";
+    String AUTHENTICATION_TOKEN_HEADER_KEY = "authentication";
 
 }
